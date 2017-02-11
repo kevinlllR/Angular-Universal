4c72048a50ed6f8ff6ebf540e0c000660ba4a94e
kevin <kevin.mendoza@unmsm.edu.pe>
2017-02-11 09:59:59 -0500

    update

---
+++ src/+app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HomeModule } from './+home/home.module';
+import { AboutModule } from './+about/about.module';
+import { TodoModule } from './+todo/todo.module';
+
+import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent, XLargeDirective } from './app.component';
+import { MaterialModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [ AppComponent, XLargeDirective ],
+  imports: [
+    SharedModule,
+    HomeModule,
+    AboutModule,
+    TodoModule,
+    AppRoutingModule,
+    MaterialModule.forRoot()
+  ]
+})
+export class AppModule {
+}
+
+export { AppComponent } from './app.component';
